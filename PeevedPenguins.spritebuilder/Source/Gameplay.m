081058cd5168296801e08652366db81dafe54d37
Yu Zhang <atomhust@gmail.com>
2015-02-10 23:21:06 -0500

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Source/Gameplay.m
+++ PeevedPenguins.spritebuilder/Source/Gameplay.m
@@ -16,6 +16,8 @@
     
     CCNode *_mouseJointNode;
     CCPhysicsJoint *_mouseJoint;
+    
+    CCNode *_contentNode;
 }
 
 // is called when CCB file has completed loading
@@ -58,7 +60,7 @@
     // ensure followed object is in visible are when starting
     self.position = ccp(0, 0);
     CCActionFollow *follow = [CCActionFollow actionWithTarget:penguin worldBoundary:self.boundingBox];
-    [self runAction:follow];
+    [_contentNode runAction:follow];
 }
 
 @end
