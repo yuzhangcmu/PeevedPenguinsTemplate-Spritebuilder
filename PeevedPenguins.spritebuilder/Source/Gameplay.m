33ee2cefc655776de3e081b455c73341daf1092c
Yu Zhang <atomhust@gmail.com>
2015-02-11 00:30:23 -0500

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Source/Gameplay.m
+++ PeevedPenguins.spritebuilder/Source/Gameplay.m
@@ -55,7 +55,11 @@
 
 - (void)releaseCatapult {
     if (_mouseJoint != nil)
-    {   
+    {
+        // releases the joint and lets the catapult snap back
+        [_mouseJoint invalidate];
+        _mouseJoint = nil;
+        
         // releases the joint and lets the penguin fly
         [_penguinCatapultJoint invalidate];
         _penguinCatapultJoint = nil;
